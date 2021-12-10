75e6b531daa51af155bf8d5e1881299e3baa9b4d
NathanMinaev <natanm@gmail.com>
2021-12-10 18:41:24 +0300

    added vue router. added home page and about page

--- dreambox-planner-gb/src/App.vue
+++ dreambox-planner-gb/src/App.vue
@@ -1,41 +1,20 @@
 <template>
-  <CardList />
-  <button @click="clickHandler">
-    {{ addTask ? "Close Add Task" : "Add Task" }}
-  </button>
-  <CardCreation v-if="addTask" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
 </template>
 
 <script>
-import CardList from "./components/Card-List.vue";
-import CardCreation from "./components/CardCreation.vue";
-
 export default {
   name: "App",
-  components: {
-    CardList,
-    CardCreation,
-  },
+  components: {},
   data() {
-    return {
-      addTask: false,
-    };
+    return {};
   },
-  methods: {
-    clickHandler() {
-      this.addTask = !this.addTask;
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ dreambox-planner-gb/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <CardList />
+  <button @click="clickHandler">
+    {{ addTask ? "Close Add Task" : "Add Task" }}
+  </button>
+  <CardCreation v-if="addTask" />
+</template>
+
+<script>
+import CardList from "@/components/CardList.vue";
+import CardCreation from "@/components/CardCreation.vue";
+
+export default {
+  name: "Home",
+  components: {
+    CardList,
+    CardCreation,
+  },
+  data() {
+    return {
+      addTask: false,
+    };
+  },
+  methods: {
+    clickHandler() {
+      this.addTask = !this.addTask;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
