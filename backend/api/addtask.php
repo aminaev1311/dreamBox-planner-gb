549dd3c2245c911de4fc03c65a4cd808e12a44c5
Rome44100 <rome44100@gmail.com>
2021-12-13 16:02:05 +0300

    Add task add, add checking data before add.

--- backend/api/addtask.php
+++ backend/api/addtask.php
@@ -2,14 +2,40 @@
 require_once '../core/display_errors.php';
 require_once '../core/classes/dream_db.php';
 
-$dr_db = new Dream_DB();
+$res_data = array('id' => '', 'errors' => '');
 
-$ins_ar = [
-    'title' => 'Моя мечта',
-    'text'  => 'Хочу совершить восхождение на Эверест!',
-    'deadline' => '2022-04-11 08:00:00'
-];
+$front_data = file_get_contents('php://input');
 
-// $ins = $dr_db->insert_row($ins_ar);
+if (empty($front_data)) {
+    $res_data['errors'] = 'Empty data!';
+} else {
 
+    if (strpos($front_data, '=')) {
+        
+        $front_data_arr = explode('=', $front_data);
+
+        $json = json_decode($front_data_arr[1]);
+
+        if (!empty($json->title)) {
+            $ins_ar = [
+                'title' => $json->title,
+                'text'  => $json->text,
+                'deadline' => $json->deadline
+            ];
+
+            $dr_db = new Dream_DB();
+
+            $res_data['id'] = $dr_db->insert_row($ins_ar);
+        } else {
+            $res_data['errors'] = 'Empty title!';
+        }
+    } else {
+        // copy prev code
+    }
+}
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+echo json_encode($res_data, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT);
+exit;
 ?>
--- backend/api/tasks.php
+++ backend/api/tasks.php
@@ -6,6 +6,7 @@ $dr_db = new Dream_DB();
 
 $res = $dr_db->get_rows();
 
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 // echo'<pre>';print_r(json_encode($res, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT));echo'<pre>';
 echo json_encode($res, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT);
