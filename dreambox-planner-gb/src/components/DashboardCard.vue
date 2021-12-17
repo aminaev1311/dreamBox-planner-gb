463dc322eac86cb736f4c25c04e1286a93f0df08
NathanMinaev <natanm@gmail.com>
2021-12-17 10:02:05 +0300

    staged DashboardCard and Test components to git

---
+++ dreambox-planner-gb/src/components/DashboardCard.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="task" :class="{ active: isChecked }">
+    <div class="task-header">
+      <div
+        class="task-check"
+        @click="clickHandler"
+        :class="{ active: isChecked }"
+      >
+        <font-awesome-icon :icon="['far', 'check-circle']" />
+      </div>
+      <a class="task-title" v-on:click="openCard(task.id)">
+        {{ task.title }}
+      </a>
+    </div>
+
+    <p class="task-date">
+      {{ task.deadline }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  props: {
+    task: {
+      type: Object,
+    },
+  },
+  name: "DashboardCard",
+  components: {},
+  data() {
+    return {
+      isChecked: false,
+      taskForView: {},
+    };
+  },
+  computed: {
+    ...mapGetters(["getTasks"]),
+  },
+  methods: {
+    clickHandler() {
+      console.log(this.isChecked);
+      this.isChecked = !this.isChecked;
+    },
+    openCard(id) {
+      this.taskForView = this.getTasks.find((task) => {
+        return task.id === id;
+      });
+      const cardView = document.getElementById("cardView");
+      cardView.style.display = "block";
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.active
+  background-color: green
+.done
+  background-color: green
+.task
+  margin-bottom: 10px
+  background-color: white
+  border-radius: 4px
+  padding: 4px
+
+  &-header
+    display: flex
+
+  &-title
+    font-style: normal
+    font-weight: normal
+    font-size: 12px
+    line-height: 14px
+    color: #000000
+    text-align: start
+    margin-bottom: 10px
+
+    &:hover
+      text-decoration: underline
+
+  &-check
+    font-size: 16px
+    line-height: 18px
+    color: #B3B3B3
+    margin-right: 6px
+
+    &:hover
+      color: black
+
+  &-date
+    text-align: end
+    font-size: 10px
+    line-height: 12px
+    color: #B3B3B3
+</style>
---
+++ dreambox-planner-gb/src/views/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Test your ideas here!</h1>
+  <h3>
+    add your components below in the Test Component in View folder. Don't forget
+    to add them to components object below.
+  </h3>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  components: {},
+};
+</script>
+
+<style></style>
