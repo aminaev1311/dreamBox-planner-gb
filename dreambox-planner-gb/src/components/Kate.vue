c4e510cf84a846aac674dd0d30e1cf2feab6b512
NathanMinaev <natanm@gmail.com>
2021-12-17 10:57:42 +0300

    added a test component for Kate

---
+++ dreambox-planner-gb/src/components/Kate.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Kate</h1>
+  
+</template>
+
+<script>
+export default {
+  name: "Kate",
+};
+</script>
+
+<style></style>
--- dreambox-planner-gb/src/views/Test.vue
+++ dreambox-planner-gb/src/views/Test.vue
@@ -4,12 +4,14 @@
     add your components below in the Test Component in View folder. Don't forget
     to add them to components object below.
   </h3>
+  <Kate />
 </template>
 
 <script>
+import Kate from "@/components/Kate.vue";
 export default {
   name: "Test",
-  components: {},
+  components: { Kate },
 };
 </script>
 
