5c9f9eb74f006d3149adad6f1494c506456a4d1a
Rome44100 <rome44100@gmail.com>
2022-01-25 19:08:05 +0300

    Complete voice recognition.

--- backend/temp/audio.php
+++ backend/temp/audio.php
@@ -10,19 +10,65 @@ use Google\Cloud\Speech\V1\RecognitionAudio;
 use Google\Cloud\Speech\V1\RecognitionConfig;
 use Google\Cloud\Speech\V1\RecognitionConfig\AudioEncoding;
 
-$client = new SpeechClient();
+/** Uncomment and populate these variables in your code */
+// $audioFile = $_SERVER['DOCUMENT_ROOT'] . '/temp/508bad61ba9a37fa9db3d3b135dc9bf5.wav';
 
-$client->close();
+// change these variables if necessary
+$encoding = AudioEncoding::OGG_OPUS;
+$languageCode = 'ru-RU';
+// $sampleRateHertz = 8000;
+// $sampleRateHertz = 12000;
+// $sampleRateHertz = 16000;
+// $sampleRateHertz = 24000;
+$sampleRateHertz = 48000;
 
-// $uploadDir = $_SERVER['DOCUMENT_ROOT'] . '/temp/';
-// $typeFile = explode('/', $_FILES['voice']['type']);
-// $fileName = basename(md5($_FILES['voice']['tmp_name'] . time()) . '.' . $typeFile[1]);
-// $uploadFile = $uploadDir . $fileName;
+if (!empty($_FILES['voice']['type'])) {
+    $uploadDir = $_SERVER['DOCUMENT_ROOT'] . '/temp/';
+    $typeFile = explode('/', $_FILES['voice']['type']);
+    $fileName = basename(md5($_FILES['voice']['tmp_name'] . time()) . '.' . $typeFile[1]);
+    $uploadFile = $uploadDir . $fileName;
+}
 
-// if (move_uploaded_file($_FILES['voice']['tmp_name'], $uploadFile)) {
-//     $response = ['result'=>'OK', 'data'=>'/temp/' . $fileName];
-// } else {
-//     $response = ['result'=>'ERROR', 'data'=>''];
-// }
-// echo json_encode($response);
+if (!empty($_FILES['voice']['tmp_name']) && move_uploaded_file($_FILES['voice']['tmp_name'], $uploadFile)) {
+    // get contents of a file into a string
+    $content = file_get_contents($uploadFile);
+
+    // set string as audio content
+    $audio = (new RecognitionAudio())
+        ->setContent($content);
+
+    // set config
+    $config = (new RecognitionConfig())
+        ->setEncoding($encoding)
+        ->setSampleRateHertz($sampleRateHertz)
+        ->setLanguageCode($languageCode);
+
+    // create the speech client
+    $client = new SpeechClient();
+
+    try {
+        $response = $client->recognize($config, $audio);
+        // echo"<pre>";print_r($response->getResults());echo"</pre>";
+        foreach ($response->getResults() as $result) {
+            $alternatives = $result->getAlternatives();
+            $mostLikely = $alternatives[0];
+            $transcript = $mostLikely->getTranscript();
+            // $confidence = $mostLikely->getConfidence();
+            // printf('Transcript: %s' . PHP_EOL, $transcript);
+            // printf('Confidence: %s' . PHP_EOL, $confidence);
+
+            $response = ['result'=>'OK', 'data'=>strval($transcript)];
+        }
+
+        if (file_exists($uploadFile)) {
+            unlink($uploadFile);
+        }
+    } finally {
+        $client->close();
+    }
+
+} else {
+    $response = ['result'=>'ERROR', 'data'=>'No file send!'];
+}
+echo json_encode($response);
 ?>
